3011da44823baa5d7b503c45dc7ee55b5753d6c3
IKS-cod <ik.smirnov@yandex.ru>
2024-11-24 17:25:57 +0300

    Initial commit

---
+++ src/test/java/TW2/Tw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package TW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
