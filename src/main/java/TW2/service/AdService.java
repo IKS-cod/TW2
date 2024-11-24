5dffad995d9a6df2a407de35a72b6847c6baa712
Katrin_skt <katrinpivzavod@mail.ru>
2024-11-24 18:47:22 +0300

    Выполнила задачу #TW-10
    Создала классы AdController и AdService, применила документирующие аннотации Swagger

---
+++ src/main/java/TW2/service/AdService.java
@@ -0,0 +1,28 @@
+package TW2.service;
+
+import TW2.dto.Ad;
+import TW2.dto.Ads;
+import TW2.dto.CreateOrUpdateAd;
+import TW2.dto.ExtendedAd;
+import org.springframework.web.multipart.MultipartFile;
+
+public class AdService {
+    public Ads getAllAds() {
+        return null;
+    }
+
+    public Ad addAd(MultipartFile image, CreateOrUpdateAd ad) {
+        return null;
+    }
+
+    public ExtendedAd getAdById(Integer id) {
+        return null;
+    }
+
+    public void updateAd(Integer id, CreateOrUpdateAd ad) {
+    }
+
+    public void removeAd(Integer id) {
+    }
+
+}
