7e924b5f201421acfd32c4571328c83f7e9b5ce0
IKS-cod <ik.smirnov@yandex.ru>
2024-12-06 20:46:54 +0300

    Сделал задачу TW-59
    Исправил класс AdsRepository , написал javadoc, логирование

--- src/main/java/TW2/repository/AdsRepository.java
+++ src/main/java/TW2/repository/AdsRepository.java
@@ -5,6 +5,24 @@ import TW2.model.Ads;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+/**
+ * Репозиторий для работы с сущностями объявлений.
+ * <p>
+ * Этот интерфейс предоставляет методы для выполнения операций CRUD
+ * с объявлениями в базе данных, а также дополнительные методы поиска.
+ * </p>
+ */
 @Repository
-public interface AdsRepository extends JpaRepository<Ads,Integer> {
+public interface AdsRepository extends JpaRepository<Ads, Integer> {
+
+    /**
+     * Находит список объявлений по идентификатору пользователя.
+     *
+     * @param usersId уникальный идентификатор пользователя.
+     * @return список объявлений, принадлежащих указанному пользователю.
+     */
+    List<Ads> findByUsersId(Integer usersId);
 }
